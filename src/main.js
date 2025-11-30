ce7e9fc46bfe4062f908dbf9f096ceb14a13469b
Maxim <maximb7474@gmail.com>
2025-11-30 16:50:18 +0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+//Подключение стилей
+import './style/main.css';
+import './style/root.css';
+
+//Создание приложения
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app')
+
